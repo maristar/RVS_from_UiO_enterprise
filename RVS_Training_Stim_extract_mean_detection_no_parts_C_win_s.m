4dadc211dd4539da4fc05084a51400c377a34b11
Maria Stavrinou <maria.stavrinou@psykologi.uio.no>
2017-02-23 16:26:06 +0100

    corrected bug about number of channels to include

--- RVS_Training_Stim_extract_mean_detection_no_parts_C_win_s.m
+++ RVS_Training_Stim_extract_mean_detection_no_parts_C_win_s.m
@@ -1,4 +1,7 @@
-%% To do the grandaverage plots for the RVS_Training
+% Program to detect the peak and their mean values in an interval of 50
+% msec around them. This is used in Training and Stim when no parts (4
+% blocks). 
+% Revision history
 % Maria Stavrinou
 % 14 June 2016 under construction
 % 08.09.2016. Changing the previous program,
@@ -20,19 +23,18 @@ global Raw_path Analyzed_path folder_data_save folder_figures_save temp22 ...
     type_files_identifier new_pre_trigger new_post_trigger numchans ...
     chanlocs header_raw 
 
-% Or make a class, or make a structure
-
+% Make a structure to save all info on dataset and analysis
 data_Properties.scope='analysis  of stim training no parts and 4 rew levels'
 data_Properties.date=date;
-data_Properties.Raw_path=Raw_path;
-data_Properties.Analyzed_path=Analyzed_path;
+
 %% Define new directories to save data and figures
 Raw_path='Y:\Prosjekt\RVS_43_subjects\Raw_datasets\DataRVS\';
 Analyzed_path='Y:\Prosjekt\RVS_43_subjects\Analyzed_datasets\';
-
+data_Properties.Raw_path=Raw_path;
+data_Properties.Analyzed_path=Analyzed_path;
 
 % Directory for data
-folder_data_save='Results_Training_Stim_noparts_accLuck_43subjects';
+folder_data_save='Results_Training_Stim_noparts_accLuck_43subjects_test';
 cd(Analyzed_path)
 mkdir(folder_data_save)
 
@@ -70,15 +72,16 @@ conditions={'stim_20L_corr', 'stim_50H_corr', 'stim_50L_corr', 'stim_80H_corr'};
 
 conditions_short={'20L', '50H', '50L', '80H'};
 
-% Parts
+%%  Parts
 % For analysis with 4 parts 
-part_names_all={'part_a'; 'part_b'; 'part_c'; 'part_d'};
+% part_names_all={'part_a'; 'part_b'; 'part_c'; 'part_d'};
 
 % For analysis with one part only 
 part_names_all={};
 
 
 data_Properties.conditions=conditions;
+data_Properties.conditions_short=conditions_short;
 data_Properties.part_names_all=part_names_all;
 
 %% Define the header for the excel file
@@ -118,12 +121,17 @@ end
 clear kk jj
 
 data_Properties.header_raw=header_raw;
-%% Define which subjects to keep in the analysis for FRN here
+%% Define which subjects to keep in the analysis 
 
 % bad_subject_list=[6,8,16,18,22,26,32,34,37,40]; % FRN
 %bad_subject_list=[6,8,13,14,15,16,18,19,22,26,28]; % for Stim
 bad_subject_list=[1, 4, 8, 18, 22, 26, 30]; % for Stim added 28 
-good_subj_list=[]; for kk=1:Num_folders, if ~ismember(kk, bad_subject_list), good_subj_list=[good_subj_list kk]; end; end
+good_subj_list=[]; 
+for kk=1:Num_folders,
+    if ~ismember(kk, bad_subject_list), 
+        good_subj_list=[good_subj_list kk]; 
+    end; 
+end
 
 data_Properties.bad_subject_list=bad_subject_list;
 data_Properties.good_subj_list=good_subj_list;
@@ -131,7 +139,14 @@ data_Properties.good_subj_list=good_subj_list;
 
 % For FRN numchans=[29, 32, 38, 47, 48];
 numchans=[29, 30, 31, 32, 33, 38, 47, 48];
-% So they are: 29: Oz; 31: Pz; 32: CPZ; 33: Fpz; 38: Fz; 47: FCz; 48: Cz; 
+% So they are: 
+% 29: Oz; 
+% 31: Pz; 
+% 32: CPZ; 
+% 33: Fpz; 
+% 38: Fz; 
+% 47: FCz; 
+% 48: Cz; 
 
 % For Stim to choose parietal and occipital electrodes. but can start with
 % the same as FRn TODO 
@@ -144,7 +159,7 @@ new_post_trigger=700;
 data_Properties.new_pre_trigger=new_pre_trigger;
 data_Properties.new_post_trigger=new_post_trigger;
 
-
+%% Start!
 startfolder=1;
 for mkk=startfolder:length(good_subj_list)
     jjk=good_subj_list(mkk);
@@ -166,7 +181,8 @@ for mkk=startfolder:length(good_subj_list)
             % Go the Analyzed_path_folder for each subject
             % and search for the set files for each AX, AY condition
             cd(Analyzed_path_folder)
-            type_files_identifier='*_256__Luck_stim_';
+            type_files_identifier='*_256__Luck_stim_'; 
+            
             Search_for_folder=[type_files_identifier temp_condition_char '.set'];
             listing_sets=dir(Search_for_folder);
             Num_setfiles=length(listing_sets);
@@ -180,85 +196,83 @@ for mkk=startfolder:length(good_subj_list)
              end
              
              % TODO LATER
-%              if length(part_names_all)>0
-%                 for gg=1:2
-%                 part_name_temp=part_names{gg};
-%                 part_name_temp_char=char(part_name_temp);
-%                 
-        %1
-        % Find where the condition starts in the filename
-        B=strfind(temp_sets{:}, temp_condition_char);        
-        %1
-        name1=temp_sets{:}(1:(B-1));%'Subject_103_25_512_ch_DC_epochs_tr2_autotriggers'; %AX.txt;
-        name2=[temp_condition_char]
-        name3a=[part_name_temp_char];
-        name3b='.set';
-        name_file=[name1 name2 name3a name3b]; %Subject101_Training1_512_ch_DC_epochs_50_triggers_Correct.txt3ch.set
-        name_data=[name1 name2]; %Subject101_Training1_512_ch_DC_epochs_50_triggers_Correct
-        
-        AreWeRight=strcmp(name_file, temp_sets{:});
-        if AreWeRight==1, 
-            disp(['Working on file ' temp_sets{:} ' for condition ' temp_condition_char]);
-            EEG = pop_loadset('filename',name_file,'filepath',Analyzed_path_folder);
-            EEG = eeg_checkset( EEG );
-            eeglab redraw
-            
-            % Select smaller timepoints 
-            if (jjk==good_subj_list(1) & mm==1 & kk==1)
-                Fs=EEG.srate;
-                pre_trigger = -EEG.xmin*1000; %msec  200 700
-                post_trigger = EEG.xmax*1000; %msec 1100 1600
-                data_pre_trigger = floor(pre_trigger*Fs/1000);
-                data_post_trigger = floor(post_trigger*Fs/1000);
-                timeVec = (-(data_pre_trigger):(data_post_trigger));
-                timeVec = timeVec';
-                timeVec_msec = timeVec.*(1000/Fs);
-                
-%                 new_pre_trigger=-200;
-%                 new_post_trigger=700;
-                find_new_pre_trigger=find(timeVec_msec>new_pre_trigger);
-                new_pre_trigger_index=min(find_new_pre_trigger);
-                
-                find_new_post_trigger=find(timeVec_msec<new_post_trigger);
-                new_post_trigger_index=max(find_new_post_trigger);
-                disp('Epoch new shorter duration done')
-                timeVec_msec_new=timeVec_msec(new_pre_trigger_index:new_post_trigger_index);
-                clear timeVec_msec
-                timeVec_msec=timeVec_msec_new;
-                clear timeVec_msec_new;
-            end
-            
-            % Save the EEG.data with smaller epoch
+            %              if length(part_names_all)>0
+            %                 for gg=1:2
+            %                 part_name_temp=part_names{gg};
+            %                 part_name_temp_char=char(part_name_temp);
+            %                 
+            %1
+            % Find where the condition starts in the filename
+            B=strfind(temp_sets{:}, temp_condition_char);        
+            %1
+            name1=temp_sets{:}(1:(B-1));%'Subject_103_25_512_ch_DC_epochs_tr2_autotriggers'; %AX.txt;
+            name2=[temp_condition_char]
+            name3a=[part_name_temp_char];
+            name3b='.set';
+            name_file=[name1 name2 name3a name3b]; %Subject101_Training1_512_ch_DC_epochs_50_triggers_Correct.txt3ch.set
+            name_data=[name1 name2]; %Subject101_Training1_512_ch_DC_epochs_50_triggers_Correct
 
-                
-                        data=EEG.data(:, new_pre_trigger_index:new_post_trigger_index, :);
-                        nchanGA=size(data, 1);
-                     if nchanGA>5
-                         
-                         data2=EEG.data(numchans, new_pre_trigger_index:new_post_trigger_index, :);
-                         clear data
-                         data=data2;
-                     end
-                     
-                    % Save the EEG.data with smaller epoch
-                    data=EEG.data(:, new_pre_trigger_index:new_post_trigger_index, :); 
-                    meandata=mean(data, 3);
-                    
-                    if isempty(part_name_temp_char)
-                        disp('Doing for non parts')
-                        part_name_temp_char='allparts'
-                    end
-                    
-                    Mean_Subjects.(Folder_name).(temp_condition_char).(part_name_temp_char)=meandata;
+            AreWeRight=strcmp(name_file, temp_sets{:});
+            if AreWeRight==1, 
+                disp(['Working on file ' temp_sets{:} ' for condition ' temp_condition_char]);
+                EEG = pop_loadset('filename',name_file,'filepath',Analyzed_path_folder);
+                EEG = eeg_checkset( EEG );
+                eeglab redraw
+
+                % Select smaller timepoints % To delete it in the future
+                % and use the ones we have already. 
+                if (jjk==good_subj_list(1) & mm==1 & kk==1)
+                    Fs=EEG.srate;
+                    pre_trigger = -EEG.xmin*1000; %msec  200 700% 23.02.2017, EEG.xmin=-199.2188,so we do not need the minus
+                    post_trigger = EEG.xmax*1000; %msec 1100 1600
+                    data_pre_trigger = floor(pre_trigger*Fs/1000);
+                    data_post_trigger = floor(post_trigger*Fs/1000);
+                    timeVec = (-data_pre_trigger:data_post_trigger);
+                    timeVec = timeVec';
+                    timeVec_msec = timeVec.*(1000/Fs);
+
+                    new_pre_trigger=-200; % why negative here?
+                    new_post_trigger=700;
+
+                    find_new_pre_trigger=find(timeVec_msec>new_pre_trigger);
+                    new_pre_trigger_index=min(find_new_pre_trigger);
+
+                    find_new_post_trigger=find(timeVec_msec<new_post_trigger);
+                    new_post_trigger_index=max(find_new_post_trigger);
+                    disp('Epoch new shorter duration done')
+                    timeVec_msec_new=timeVec_msec(new_pre_trigger_index:new_post_trigger_index);
+                    clear timeVec_msec
+                    timeVec_msec=timeVec_msec_new;
+                    clear timeVec_msec_new;
+                end
+
+                % Save the EEG.data with smaller epoch
+                data=EEG.data(:, new_pre_trigger_index:new_post_trigger_index, :);
+                nchanGA=size(data, 1);
+                if nchanGA>5
+                    data2=EEG.data(numchans, new_pre_trigger_index:new_post_trigger_index, :);
                     clear data
-        end % if we are right 
-              
-                end   % for gg
-            end % if length part_names_all >0
-    end % Sessions 
-%end % Subject
-% chanlocs=EEG.chanlocs; 
+                    data=data2;
+                end
 
+                % Average with the number of trials. 
+                meandata=mean(data, 3);
+
+                % Give a name to the part_name_temp_char
+                if isempty(part_name_temp_char)
+                    disp('Doing for non parts')
+                    part_name_temp_char='allparts'
+                end
+
+                % Save the results
+                Mean_Subjects.(Folder_name).(temp_condition_char).(part_name_temp_char)=meandata;
+                clear data
+            end % For "if we are right"
+        end   % For conditions
+    end % For Sessions 
+end % For Subjects 
+
+% Save the channels names
 chanlocs=EEG.chanlocs(numchans); 
 data_Properties.chanlocs=chanlocs;
 
@@ -276,6 +290,8 @@ save chanlocs chanlocs
 data_Properties.Analyzed_path=Analyzed_path;
 data_Properties.timeVec_msec=timeVec_msec;
 data_Properties.Fs=Fs;
+data_Properties.new_pre_trigger=new_pre_trigger;
+data_Properties.new_post_trigger=new_post_trigger;
 save data_Properties data_Properties
 
 % %Check if we need any of this
@@ -294,13 +310,24 @@ save data_Properties data_Properties
 %     temp_condition ...
 %     temp_condition_char
 
+
+%% Run the program to extract the individual peak times
+% SOS deactivate if not individual limits. 
+cd('Y:\Prosjekt\RVS_43_subjects\Analyzed_datasets\NOISY_TXT');
+run RVS_Training_Stim_extract_individual_mean_peak_intervals
+
+
 %% Define time limits for the peak detection 
 name_component='N1';
 type='mean';
-peak_start_time=interval_N1_1;
-peak_end_time=300;
+peak_start_time=[];%250
+peak_end_time=[];%300;
 time_start=new_pre_trigger; %was -200 % MLS 08.09.2+16 changed % In msec -there is abs(time_start) in the function so the minus is disgarted
 time_end=new_post_trigger; %600; % TODO sth here why it is deleted 
+
+name_interval=['interval_' name_component];
+interval_temp=eval(name_interval);
+data_Properties.(name_interval)=interval_temp;
 
 [ Peak_results, Tnew] = peak_detection_all_individ( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
 
@@ -309,15 +336,23 @@ time_end=new_post_trigger; %600; % TODO sth here why it is deleted
 %% Search for the FRN or N2(Stim) : 250-300 msec. 
 % Define time limits for the peak detection 
 name_component='N2';
-type='base_peak';
-peak_start_time=250;
-peak_end_time=300;
+type='mean';
+peak_start_time=[];%250;
+peak_end_time=[];%300;
 time_start=new_pre_trigger; %was -200 % MLS 08.09.2+16 changed % In msec -there is abs(time_start) in the function so the minus is disgarted
 time_end=new_post_trigger; %600; % TODO sth here why it is deleted 
 
-[ Peak_results, Tnew] = peak_detection_all( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+name_interval=['interval_' name_component];
+interval_temp=eval(name_interval);
+data_Properties.(name_interval)=interval_temp;
+
+[ Peak_results, Tnew] = peak_detection_all_individ( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+
+
+%[ Peak_results, Tnew] = peak_detection_all( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
 
 clear Peak_results Tnew
+
 %% P300 -a detection 
 % Define time limits for the peak detection 
 name_component='P3a';
@@ -327,7 +362,15 @@ peak_end_time=420;
 time_start=new_pre_trigger; %was -200 % MLS 08.09.2+16 changed % In msec -there is abs(time_start) in the function so the minus is disgarted
 time_end=new_post_trigger;
 % In msec 
-[ Peak_results, Tnew] = peak_detection_all( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+
+name_interval=['interval_' name_component];
+interval_temp=eval(name_interval);
+data_Properties.(name_interval)=interval_temp;
+
+[ Peak_results, Tnew] = peak_detection_all_individ( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+
+
+%[ Peak_results, Tnew] = peak_detection_all( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
 clear Peak_results Tnew
 
 %% P300 -b detection 
@@ -339,7 +382,15 @@ peak_end_time=550;
 time_start=new_pre_trigger; %was -200 % MLS 08.09.2+16 changed % In msec -there is abs(time_start) in the function so the minus is disgarted
 time_end=new_post_trigger;
 % In msec 
-[ Peak_results, Tnew] = peak_detection_all( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+
+name_interval=['interval_' name_component];
+interval_temp=eval(name_interval);
+data_Properties.(name_interval)=interval_temp;
+
+[ Peak_results, Tnew] = peak_detection_all_individ( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+
+
+%[ Peak_results, Tnew] = peak_detection_all( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
 clear Peak_results Tnew
 
 
@@ -351,8 +402,41 @@ peak_end_time=490;
 time_start=new_pre_trigger; %was -200 % MLS 08.09.2+16 changed % In msec -there is abs(time_start) in the function so the minus is disgarted
 time_end=new_post_trigger; %600; % TODO sth here why it is deleted 
 
-[ Peak_results, Tnew] = peak_detection_all( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+name_interval=['interval_' name_component];
+interval_temp=eval(name_interval);
+data_Properties.(name_interval)=interval_temp;
+
+[ Peak_results, Tnew] = peak_detection_all_individ( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+
+
+%[ Peak_results, Tnew] = peak_detection_all( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
 
 clear Peak_results Tnew
+
+%% P2 
+%% P2 -a detection 
+% Define time limits for the peak detection 
+name_component='P2';
+type='mean';
+peak_start_time=[];
+peak_end_time=[];
+time_start=new_pre_trigger; %was -200 % MLS 08.09.2+16 changed % In msec -there is abs(time_start) in the function so the minus is disgarted
+time_end=new_post_trigger;
+% In msec 
+
+name_interval=['interval_' name_component];
+interval_temp=eval(name_interval);
+data_Properties.(name_interval)=interval_temp;
+
+[ Peak_results, Tnew] = peak_detection_all_individ( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+
+
+%[ Peak_results, Tnew] = peak_detection_all( name_component, type, peak_start_time, peak_end_time, time_start, time_end, Mean_Subjects, data_Properties )
+clear Peak_results Tnew
+
+%% Save data properties
+cd(Analyzed_path)
+cd(folder_data_save)
+save data_Properties data_Properties
 
 toc
